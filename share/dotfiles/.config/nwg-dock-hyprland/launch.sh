66ffcdea6e8e4223f85f5963801d59e1870ecff3
Josh Munoz <laparkaone@yahoo.com>
2024-12-06 15:28:17 -0600

    update

--- share/dotfiles/.config/nwg-dock-hyprland/launch.sh
+++ share/dotfiles/.config/nwg-dock-hyprland/launch.sh
@@ -4,6 +4,4 @@
 # /____/\___/\__/_/\_\ 
 #                      
 
-if [ -f $HOME/.config/ml4w/settings/nwg-dock-hyprland.sh ] ;then
     nwg-dock-hyprland -lp start -i 32 -w 10 -mb 10 -ml 10 -mr 10 -x -c "rofi -show drun"
-fi
